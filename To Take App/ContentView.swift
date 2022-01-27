c8646767e7a0c1c4c722a6956c21aeb3dfceec1d
Jamie <jxxthomson@gmail.com>
2022-01-27 18:10:48 +0000

    Initial Commit

---
+++ To Take App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  To Take App
+//
+//  Created by Jamie Thomson on 27/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ To Take App/To_Take_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Take_AppApp.swift
+//  To Take App
+//
+//  Created by Jamie Thomson on 27/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct To_Take_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
